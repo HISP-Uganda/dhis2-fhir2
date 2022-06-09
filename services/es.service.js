3b10aa7661f848bfafc84a3034447064e8435669
Charles Olupot <olupotcharles@gmail.com>
2022-06-09 13:12:30 +0300

    Change the url

--- services/es.service.js
+++ services/es.service.js
@@ -1,8 +1,8 @@
 "use strict";
 const { Client } = require("@elastic/elasticsearch");
 
-// const client = new Client({ node: "http://localhost:9200" });
-const client = new Client({ node: "http://192.168.64.3:9200" });
+const client = new Client({ node: "http://localhost:9200" });
+// const client = new Client({ node: "http://192.168.64.3:9200" });
 
 require("array.prototype.flatmap").shim();
 
